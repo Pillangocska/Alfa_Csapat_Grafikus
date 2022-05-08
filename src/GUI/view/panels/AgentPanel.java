d756c57a42e9a57e419bf6e3621e972282fb2aff
fay-ambrus <ambrus.fay@gmail.com>
2022-05-08 23:11:16 +0200

    AgentPanel 1.0

--- src/GUI/view/panels/AgentPanel.java
+++ src/GUI/view/panels/AgentPanel.java
@@ -2,8 +2,24 @@ package GUI.view.panels;
 
 import GUI.view.view.AgentView;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class AgentPanel extends BaseBagPanel {
     private AgentView view;
+    private JLabel imageLabel;
 
+    public AgentPanel() {
+        ImageIcon imageIcon = new ImageIcon("resources/logo.png");
+        imageIcon.setImage(imageIcon.getImage().getScaledInstance(35, 35, Image.SCALE_SMOOTH));
+        imageLabel = new JLabel(imageIcon);
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        imageLabel.setVerticalAlignment(SwingConstants.CENTER);
+        add(imageLabel);
+    }
 
+    @Override
+    public void update() {
+
+    }
 }
