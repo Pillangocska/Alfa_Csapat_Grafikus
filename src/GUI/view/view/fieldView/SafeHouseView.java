aef18ab34d7d46588f5c0101cb8dd1670b0b353c
Fejes Kristóf <kristi199908@gmail.com>
2022-05-11 22:40:19 +0200

    SafeHose image

--- src/GUI/view/view/fieldView/SafeHouseView.java
+++ src/GUI/view/view/fieldView/SafeHouseView.java
@@ -10,11 +10,19 @@ public class SafeHouseView extends FieldView{
     private ArrayList<EquipmentView> equipments = new ArrayList<>();
 
     public SafeHouseView(){
-        color = Color.orange;
-        newImage = Toolkit.getDefaultToolkit().createImage("resources/cloak.png");
-        backGround = newImage.getScaledInstance(195,195,Image.SCALE_DEFAULT);
+        color = Color.getHSBColor(0,250,0);
+        newImage = Toolkit.getDefaultToolkit().createImage("resources/SafeHouse1.png");
+        backGround = newImage.getScaledInstance(200,200,Image.SCALE_DEFAULT);
         this.text = "safe";
         setFieldText("safe");
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        drawPolygon(g2d,200/2, 200/2 ,1,color.getRGB(),true);
+        g.drawImage(backGround,0,0,null);
+        this.repaint();
     }
 
     public void setField(SafeHouse safeh) {
