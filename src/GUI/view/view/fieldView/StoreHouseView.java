d53429d19241d17bd9194371665bd41031b9e12c
Fejes Kristóf <kristi199908@gmail.com>
2022-05-11 23:05:51 +0200

    StoreHouse image done

--- src/GUI/view/view/fieldView/StoreHouseView.java
+++ src/GUI/view/view/fieldView/StoreHouseView.java
@@ -9,11 +9,19 @@ public class StoreHouseView extends FieldView {
     private ElementView elementv;
 
     public StoreHouseView(){
-        color = Color.CYAN;
-        newImage = Toolkit.getDefaultToolkit().createImage("resources/bag.png");
-        backGround = newImage.getScaledInstance(195,195,Image.SCALE_DEFAULT);
+        color = Color.getHSBColor(0,250,0);
+        newImage = Toolkit.getDefaultToolkit().createImage("resources/StoreHouse1.png");
+        backGround = newImage.getScaledInstance(200,200,Image.SCALE_DEFAULT);
         this.text = "store";
         setFieldText("store");
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        drawPolygon(g2d,200/2, 200/2 ,1,color.getRGB(),true);
+        g.drawImage(backGround,0,0,null);
+        this.repaint();
     }
 
     public void setField(StoreHouse storeh) {
